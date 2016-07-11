e7e2aa4c5cdbeb83aef350e3e5a3f8f4c9a3fb17
cuongpt <cuongtuanpham159@gmail.com>
2016-07-11 17:16:15 +0700

    fix undified bug but got something else

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1800, 800, Phaser.AUTO, '', { preload: preload, create: create, update: update, fire: fire});
 var starVelocity = 50;
 var i = 0.5;
 
@@ -29,34 +29,53 @@ function fire(){
     return;
   }
   this.nextShotAt = this.time.now + this.shotDelay;
+
   this.blast = this.game.add.sprite(this.star.x, this.star.y, 'blast');
   this.blast.anchor.setTo(0.5, 0.5);
   this.physics.enable(this.blast, Phaser.Physics.ARCADE);
-  this.blast.scale.setTo(i, i);
-  i += 0.05;
+//  if (i < 2){
+    this.blast.scale.setTo(i, i);
+  //  i += 0.5;
+//  }else {this.blast.destroy()};
+
   this.blasts.push(this.blast);
+  console.log(this.blasts);
 }
 
 function update() {
   if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
     this.star.body.velocity.x -= starVelocity;
-    this.blast.body.velocity.x = this.star.body.velocity.x;
+    if(this.blast != null){
+      this.blast.body.velocity.x = this.star.body.velocity.x;
+    }
   }
   else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
     this.star.body.velocity.x += starVelocity;
-    this.blast.body.velocity.x = this.star.body.velocity.x;
+    if(this.blast != null){
+      this.blast.body.velocity.x = this.star.body.velocity.x;
+    }
   }
-  else {this.star.body.velocity.x = 0;};
+  else {this.star.body.velocity.x = 0;
+    if(this.blast != null){
+      this.blast.body.velocity.x = this.star.body.velocity.x;
+    }};
 
   if (this.game.input.keyboard.isDown(Phaser.Keyboard.UP)){
     this.star.body.velocity.y -= starVelocity;
-    this.blast.body.velocity.y = this.star.body.velocity.y;
+    if(this.blast != null){
+      this.blast.body.velocity.y = this.star.body.velocity.y;
+    }
   }
   else if (this.game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
     this.star.body.velocity.y += starVelocity;
-    this.blast.body.velocity.y = this.star.body.velocity.y;
+    if(this.blast != null){
+      this.blast.body.velocity.y = this.star.body.velocity.y;
+    }
   }
-  else {this.star.body.velocity.y = 0; };
+  else {this.star.body.velocity.y = 0;
+    if(this.blast != null){
+      this.blast.body.velocity.y = this.star.body.velocity.y;
+    }};
 
   if (this.game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)){
       this.fire();
